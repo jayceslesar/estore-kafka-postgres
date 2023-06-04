e8fbdb7b790e8fb83e5c915c34f87c561d2c85d5
jayce <jslesar@beta.team>
2023-06-04 11:31:44 -0400

    add some testing that caught a bug + actions

--- src/consumer.py
+++ src/consumer.py
@@ -1,8 +1,40 @@
 """Consumer module for code consumers use."""
 
 import json
+import uuid
 
 from kafka import KafkaConsumer
+
+
+def _reformat_message(message: dict[str, str]) -> dict[str, str]:
+    """Reformat a message consumed from kafka by coercing strings + floats into common format.
+
+    Args:
+        message: message that was consumed
+
+    Returns:
+        reformatted dict of same data but cleaner
+    """
+    reformatted = {}
+    for key, value in message.items():
+        if isinstance(value, float):
+            value = round(value, 2)
+        else:
+            value = value.replace("\n", " ")
+        reformatted[key] = value
+
+    return reformatted
+
+
+def _write_message(message: dict[str, str], write_to: str) -> None:
+    """Write a reformatted message to disk to be loaded into postgres.
+
+    Args:
+        message: reformatted message that was consumed from the topic
+        write_to: where to write
+    """
+    name = uuid.uuid4().hex
+    pass
 
 
 def consume(host: str, port: int, write_to: str) -> None:
@@ -21,4 +53,5 @@ def consume(host: str, port: int, write_to: str) -> None:
     )
 
     for message in consumer:
-        print(f"Received message: {message.value}")
+        reformatted_message = _reformat_message(message.value)
+        _write_message(reformatted_message, write_to)
---
+++ tests/test_reformat.py
@@ -0,0 +1,26 @@
+import pytest
+
+from src.consumer import _reformat_message
+
+
+@pytest.fixture()
+def message() -> dict[str, str]:
+    return {
+        'company': 'Taylor Group',
+        'first_name': 'Maria',
+        'last_name': 'Smith',
+        'phone_number': '415.891.5222',
+        'address': '8618 Lisa Via Apt. 573\nFisherfort, TX 14385',
+        'credit_card': 'Maestro\nKimberly Jones\n584192994856 09/30\nCVV: 267\n',
+        'date': '2023-06-04T07:17:48+00:00',
+        'amount': 2826.8691548518036
+    }
+
+def test_reformat_message(message: dict[str, str]) -> None:
+    reformatted_message = _reformat_message(message)
+    print(reformatted_message)
+    for key, value in reformatted_message.items():
+        if isinstance(value, str):
+            assert "\n" not in value
+        else:
+            assert len(str(value).split(".")[-1]) <= 3
